ad825807f265721dfba4193f09fa81a295f7628c
Rafael Levi <rafaellevissa@gmail.com>
2025-03-03 18:57:22 -0300

    adjusts

--- handler.mjs
+++ handler.mjs
@@ -1,42 +1,36 @@
-import mysql from 'mysql';
-var connection = mysql.createConnection({
-  host     : 'database-da-live.cfukc8fa9ncd.us-east-1.rds.amazonaws.com',
-  user     : 'admin',
-  password : 'admin123',
-  database : 'live'
-});
- 
-connection.connect();
+import mysql from 'mysql2/promise';
 
-export const handler = async (event) => {
-  
-  if (event.method == "GET") {
-    const result = connection.query('SELECT * from user;');
-    connection.end();
+export const handler = async (event, context) => {
+  var connection = await mysql.createConnection({
+    host     : 'database-da-live.cfukc8fa9ncd.us-east-1.rds.amazonaws.com',
+    user     : 'admin',
+    password : 'admin123',
+    database : 'live'
+  });
+  console.log(event, context, event.httpMethod);
+  if (event.requestContext.http.method == "GET") {
+    const [result] = await connection.query('SELECT * from user;');
     return {
       statusCode: 200,
       body: JSON.stringify(result),
     };
   }
-  if (event.method == "POST") {
-    connection.query('INSERT INTO user (name, email) VALUES ('+event.body.name+', '+event.body.email+');');
-    connection.end();
+  if (event.requestContext.http.method == "POST") {
+    await connection.query('INSERT INTO user (name, email) VALUES ("'+JSON.parse(event.body).name+'", "'+JSON.parse(event.body).email+'");');
     return {
       statusCode: 200,
       body: JSON.stringify('User inserted!'),
     };
   }
-  if (event.method == "PUT") {
-    connection.query('UPDATE user SET name = '+event.body.name+'WHERE email = '+event.body.email+';')
-    connection.end();
+  if (event.requestContext.http.method == "PUT") {
+    await connection.query('UPDATE user SET name = "'+JSON.parse(event.body).name+'" WHERE email = "'+JSON.parse(event.body).email+'";')
     return {
       statusCode: 200,
       body: JSON.stringify('User updated!'),
     };
   }
-  if (event.method == "DELETE") {
-    connection.query('DELETE FROM user WHERE email = '+event.body.email+';')
-    connection.end();
+  if (event.requestContext.http.method == "DELETE") {
+    await connection.query('DELETE FROM user WHERE email = "'+JSON.parse(event.body).email+'";')
     return {
       statusCode: 200,
       body: JSON.stringify('User deleted!'),
